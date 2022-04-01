d282f184659f949da32207d3069670ae6a40bb12
LinGaOscar <oscar987123@yahoo.com.tw>
2022-04-01 11:31:30 +0800

    Update GetAllUser.java

--- MariaServlet/src/main/java/com/oscar/db/GetAllUser.java
+++ MariaServlet/src/main/java/com/oscar/db/GetAllUser.java
@@ -1,4 +1,5 @@
 package com.oscar.db;
+
 import java.sql.*;
 import java.io.IOException;
 
@@ -14,49 +15,44 @@ import javax.sql.DataSource;
 @WebServlet("/GetAllUser")
 public class GetAllUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	Connection conn = null;
+
 	private static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
-//	private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String DB_URL = "jdbc:maria://localhost:3307/the_round_table";
-//	private static final String DB_URL = "jdbc:mysql://localhost:3306/jdbc?serverTimezone=UTC";
-	private static final String USER = "root";
-	private static final String PASSWORD = "07310731";
+	private static final String DB_URL = "jdbc:mariadb://localhost:3307/the_round_table?user=root&password=07310731";
 
 	private static final String SQLUPD = "UPDATE employee SET salary = ? WHERE empno = ?";
 	private static final String SQLSEL = "SELECT * FROM sys_user";
-	
-    public GetAllUser() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//	http://localhost:8080/MariaServlet/GetAllUser
+	public GetAllUser() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
-			Class.forName ("org.mariadb.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3307/the_round_table?user=root&password=07310731");
-//			conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+			Class.forName(JDBC_DRIVER);
+			Connection connection = DriverManager.getConnection(DB_URL);
 			PreparedStatement stmt = connection.prepareStatement(SQLSEL);
 			ResultSet rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
-				response.getWriter().append(rs.getString("id"));
-				response.getWriter().append(rs.getString("account"));
-				response.getWriter().append(rs.getString("password"));
-				response.getWriter().append(rs.getString("user_name"));
+				response.getWriter().println("id : " + rs.getString("id"));
+				response.getWriter().println("account : " + rs.getString("account"));
+				response.getWriter().println("password : " + rs.getString("password"));
+				response.getWriter().println("user_name : " + rs.getString("user_name"));
+				response.getWriter().println();
 			}
-			
-			response.getWriter().append("Served at: ").append(request.getContextPath());
+
+			response.getWriter().println("Served at: " + request.getContextPath());
 			connection.close();
 			stmt.close();
 		} catch (Exception e) {
-			// TODO: handle exception
 			System.out.println(e);
 		}
-		
-		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
